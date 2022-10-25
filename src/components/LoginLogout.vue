2b7bf27ad17ed521a341fba76fb279885a3ee136
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-10-25 11:35:58 +0200

    Création Login Logout

---
+++ src/components/LoginLogout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { supabase, user } from '../supabase';
+</script>
+<template>
+<div>
+    <button v-if="user" @pointerdown="supabase.auth.signOut()">
+    Se déconnecter ({{user.email}})
+    </button>
+    <button v-else @pointerdown="supabase.auth.signIn({provider: 'facebook'})">
+    Se connecter par Facebook
+    </button>
+    <button v-else @pointerdown="supabase.auth.signIn({provider: 'google'})">
+    Se connecter par Google
+    </button>
+</div>
+</template>
