4f25bc9e4bce59e1336f0815f14d9ccda7c35036
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-10-25 10:38:11 +0200

    Import code tables BDD

--- bdd.sql
+++ bdd.sql
@@ -1,14 +1,23 @@
 --
 -- code pour la création des tables
 --
+CREATE TABLE materiaux (
+id_materiaux uuid not null default uuid_generate_v4() ,
+libelle_materiaux text, 
+    PRIMARY KEY (id_materiaux)
+);
 
-
-
-
-
-
-
-
+CREATE TABLE Montre (
+id uuid not null default uuid_generate_v4() ,
+bracelet text,
+boitier text,
+ecran text,
+id_materiaux uuid ,
+Commander boolean not null default false,
+utilisateur uuid references auth.users not null default uid(),
+primary key (id) ,
+FOREIGN KEY (id_materiaux) REFERENCES materiaux(id_materiaux)
+);
 --
 -- code pour la création des vues
 --
