3056deaf979a14eb7ba677c116ce146877f9af3f
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-10-26 08:53:00 +0200

    Mise en forme tagline

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,19 @@ import Footer from './components/Footer.vue';
 
 <template>
   <Header />
-  
-  
+  <div class="bg-Noir flex justify-between px-4">
+    <div class="px-[7%]">
+      <p class="lg:pt-10 py-2  font-made-okine-sans-personal-use font-regular text-Blanc sm:text-xl lg:text-2xl xl:text-3xl">Tik.Tak.</p>
+      <p class="lg:pt-10 py-2  font-made-okine-sans-personal-use font-light text-Blanc sm:text-lg lg:text-xl xl:text-2xl">Un modèle</p>
+      <p class="lg:pt-10 py-2  font-made-okine-sans-personal-use font-light text-Blanc sm:text-lg lg:text-xl xl:text-2xl">Une identité</p>
+      <p class="lg:pt-10 py-2  font-made-okine-sans-personal-use font-light text-Blanc sm:text-lg lg:text-xl xl:text-2xl">Votre histoire</p>
+    </div>
+    <img class="w-1/2 h-1/2 p-10  " src="./components/img/Illustration-1.png" alt="">
+  </div>
+
+
 
   <Footer />
-
-  <!-- Affiche les pages -->
   <Suspense>
     <router-view class="m-2 border-2 p-2" />
   </Suspense>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,19 +3,19 @@
 
 <template>
   <footer class="bg-Noir">
-    <div class="grid grid-cols-5 pl-6">
+    <div class="grid grid-cols-5 pl-5">
       <div class="grid grid-rows-5">
-        <p class="pt-10 pl-5 font-made-okine-sans-personal-use font-regular text-Blanc sm:text-base lg:text-base xl:text-lg">Tik.Tak.</p>
-        <p class="pt-5 pl-5 font-made-okine-sans-personal-use font-light text-Blanc sm:text-base lg:text-base xl:text-base">Un modèle</p>
-        <p class="pl-5 pt-2 font-made-okine-sans-personal-use font-light text-Blanc sm:text-base lg:text-base xl:text-base">Une identité</p>
-        <p class="pl-5 font-made-okine-sans-personal-use font-light text-Blanc sm:text-base lg:text-base xl:text-base">Votre histoire</p>
+        <p class="pt-10  font-made-okine-sans-personal-use font-regular text-Blanc sm:text-base lg:text-base xl:text-lg">Tik.Tak.</p>
+        <p class="pt-5  font-made-okine-sans-personal-use font-light text-Blanc sm:text-base lg:text-base xl:text-base">Un modèle</p>
+        <p class="pt-2 font-made-okine-sans-personal-use font-light text-Blanc sm:text-base lg:text-base xl:text-base">Une identité</p>
+        <p class="font-made-okine-sans-personal-use font-light text-Blanc sm:text-base lg:text-base xl:text-base">Votre histoire</p>
         <img src="./img/Logo-Copyright.png" alt="Logo Copyright">
       </div>
       <RouterLink to="/Collection"><p class="pt-10 pl-12 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-base lg:text-base xl:text-lg">Collection</p></RouterLink>
       <RouterLink to="/Personnalisation"><p class="pt-10 pl-10  font-made-okine-sans-personal-use font-thin text-Blanc sm:text-base lg:text-base xl:text-lg">Personnalisation</p></RouterLink>
       <RouterLink to="/Inscription"><p class="pt-10 pl-20 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-base lg:text-base xl:text-lg">Inscription</p></RouterLink>
       <div class="grid-rows-2">
-      <RouterLink to="/Connexion"><p class="pt-10 pl-12 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-base lg:text-base xl:text-lg">Connexion</p></RouterLink>
+      <RouterLink to="/Connexion"><p class="pt-10 pl-10 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-base lg:text-base xl:text-lg">Connexion</p></RouterLink>
       <p class="mt-52 font-made-okine-sans-personal-use font-light text-Blanc sm:text-base lg:text-base xl:text-base">Maxime MERCIER - 2C1</p>
       </div>
     </div>
