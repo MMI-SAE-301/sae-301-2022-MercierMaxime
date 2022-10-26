0c229e911c742c2f713273c882e48e6c461deae3
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-10-26 15:02:39 +0200

    Création champ "Prénom"

--- src/pages/inscription.vue
+++ src/pages/inscription.vue
@@ -2,10 +2,14 @@
     <div class="bg-Noir">
         <h1 class="text-Blanc text-center font-made-okine-sans-personal-use font-regular sm:text-lg lg:text-xl xl:text-4xl">Inscription</h1>
         <h2 class="text-center pt-10 pb-20 font-made-okine-sans-personal-use font-light text-Blanc sm:text-sm lg:text-base xl:text-base">Bienvenue parmis nous.</h2>
-
         <div class="flex justify-center">
             <p class="pt-3 pr-5 font-made-okine-sans-personal-use font-regular text-Blanc sm:text-sm lg:text-base xl:text-base">Nom :</p>
-            <FormKit label-class="text-Blanc" type="text" placeholder="Entrer votre nom" input-class="rounded-xl w-80 font-made-okine-sans-personal-use font-light sm:text-base lg:text-lg xl:text-xl" />
+            <FormKit label-class="text-Blanc" type="text" placeholder="Entrer votre nom" input-class="rounded-xl  font-made-okine-sans-personal-use font-light sm:text-base lg:text-lg xl:text-xl" />
+        </div>
+
+        <div class="flex justify-center pt-20">
+            <p class="pt-3 pr-3 font-made-okine-sans-personal-use font-regular text-Blanc sm:text-sm lg:text-base xl:text-base">Prénom :</p>
+            <FormKit label-class="text-Blanc" type="text" placeholder="Entrer votre prénom" input-class="rounded-xl  font-made-okine-sans-personal-use font-light sm:text-base lg:text-lg xl:text-xl" />
         </div>
     </div>
 </template>
