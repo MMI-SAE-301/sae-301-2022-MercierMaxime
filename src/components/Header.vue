d717488c3a2ea0e6ebebb72601ef336a1aafd6ed
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-10-26 11:50:48 +0200

    Route accueil via logo dans header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-Noir">
     <div class="grid grid-cols-4 space-x-4">
-      <p class="pt-10 pl-5 font-made-okine-sans-personal-use font-regular text-Blanc sm:text-base lg:text-lg xl:text-xl">Tik.Tak.</p>
+      <RouterLink to="/"><p class="pt-10 pl-5 font-made-okine-sans-personal-use font-regular text-Blanc sm:text-base lg:text-lg xl:text-xl">Tik.Tak.</p></RouterLink>
       <RouterLink to="/montre"><p class="pt-10 pl-20 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-sm lg:text-base xl:text-lg">Collection</p></RouterLink>
       <RouterLink to="/Personnalisation"><p class="pt-10 pl-3 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-sm lg:text-base xl:text-lg">Personnalisation</p></RouterLink>
       <div class="grid grid-rows-2 divide-y pl-6">
