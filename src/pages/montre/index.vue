5528ae22539469cbf1f54583b0f3e7c01967abec
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-10-26 13:28:06 +0200

    Création fonction liste vue pour tout voir

--- src/pages/montre/index.vue
+++ src/pages/montre/index.vue
@@ -7,8 +7,12 @@ import ListeMontres from "@/components/ListeMontres.vue";
   <div class="bg-Noir">
     <section v-if="user">
       <h1 class="text-Blanc text-center font-made-okine-sans-personal-use font-regular sm:text-lg lg:text-xl xl:text-4xl">Collection</h1>
-      <h2 class="text-center pt-10 font-made-okine-sans-personal-use font-light text-Blanc sm:text-sm lg:text-base xl:text-base">Vos dernières créations.</h2>
-      <ListeMontres class="flex flex-wrap gap-2" :max="3" />
+      <h2 class="text-center pt-10 pb-20 font-made-okine-sans-personal-use font-light text-Blanc sm:text-sm lg:text-base xl:text-base">Vos dernières créations.</h2>
+      <div class="flex flex-wrap">
+        <div class="w-64 m-auto grid grid-cols-3">
+        <ListeMontres class="flex flex-wrap gap-2" :max="3" />
+        </div>
+      </div>
       <RouterLink class="text-Blanc underline font-made-okine-sans-personal-use font-light" to="/montre/liste">Voir tout</RouterLink>
     </section>
   </div>
