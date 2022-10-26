44661d3ab8e124589fbee11bf9e13cee06910ce4
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-10-26 17:02:18 +0200

    Ajout nom adresse mail par connexion

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,8 @@
       <RouterLink to="/montre"><p class="pt-10 pl-20 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-sm lg:text-base xl:text-lg">Collection</p></RouterLink>
       <RouterLink to="/montre/new"><p class="pt-10 pl-3 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-sm lg:text-base xl:text-lg">Personnalisation</p></RouterLink>
       
-      <button v-if="user" @pointerdown="supabase.auth.signOut()"><p class="pt-10 pl-3 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-sm lg:text-base xl:text-lg">Déconnexion</p></button>
+      <button v-if="user" @pointerdown="supabase.auth.signOut()"><p class="pt-10 pl-3 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-sm lg:text-base xl:text-lg">Déconnexion</p>
+        <p class="font-made-okine-sans-personal-use font-thin text-Blanc sm:text-sm lg:text-sm xl:text-sm">({{user.email}})</p></button>
 
       <div v-else class="grid grid-rows-2 divide-y pl-6">
         <RouterLink to="/inscription"><p class="py-2 pl-16 font-made-okine-sans-personal-use font-thin text-Blanc sm:text-sm lg:text-base xl:text-lg">Inscription</p></RouterLink>
