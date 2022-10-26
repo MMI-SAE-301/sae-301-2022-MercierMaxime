b11bf9ab65d0c373bcdaaa6c20e6e9268958f16a
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-10-26 15:04:43 +0200

    Création champ "Adresse mail"

--- src/pages/inscription.vue
+++ src/pages/inscription.vue
@@ -11,5 +11,10 @@
             <p class="pt-3 pr-3 font-made-okine-sans-personal-use font-regular text-Blanc sm:text-sm lg:text-base xl:text-base">Prénom :</p>
             <FormKit label-class="text-Blanc" type="text" placeholder="Entrer votre prénom" input-class="rounded-xl  font-made-okine-sans-personal-use font-light sm:text-base lg:text-lg xl:text-xl" />
         </div>
+
+        <div class="flex justify-center pt-20">
+            <p class="pt-3 pr-3 font-made-okine-sans-personal-use font-regular text-Blanc sm:text-sm lg:text-base xl:text-base">Adresse mail :</p>
+            <FormKit label-class="text-Blanc" type="text" placeholder="Entrer votre email" input-class="rounded-xl  font-made-okine-sans-personal-use font-light sm:text-base lg:text-lg xl:text-xl" />
+        </div>
     </div>
 </template>
