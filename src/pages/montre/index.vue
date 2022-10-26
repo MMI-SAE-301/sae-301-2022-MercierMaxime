39f7810b35a0df1bb6ab9003971ed5aff2a7fd19
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-10-26 13:20:58 +0200

    Débit configuration collection

--- src/pages/montre/index.vue
+++ src/pages/montre/index.vue
@@ -1,29 +1,15 @@
 <script setup lang="ts">
-import MontreFace from "@/components/MontreFace.vue";
-import type { Montre } from "@/types";
 import { supabase, user } from '@/supabase';
 import ListeMontres from "@/components/ListeMontres.vue";
-
-const exemples: Montre = [{
-  bracelet: "#FFFFFF",
-  boitier: "#00FF00",
-  ecran: "FFFF00",
-},
-{
-  bracelet: "#0F532E",
-  boitier: "#00FF00",
-  ecran: "#D45FC5",
-},
-{
-  bracelet: "#FEDA34",
-  boitier: "#DFJK42",
-  ecran: "#0F532E",
-}
-];
 </script>
+
 <template>
-  <section v-if="user">
-    <h1>Un aperçu de vos Montres : (<RouterLink class="text-red-300 underline" to="/montre/liste">Voir tout</RouterLink>)</h1>
-    <ListeMontres class="flex flex-wrap gap-2" :max="3" />
-  </section>
+  <div class="bg-Noir">
+    <section v-if="user">
+      <h1 class="text-Blanc text-center font-made-okine-sans-personal-use font-regular sm:text-lg lg:text-xl xl:text-4xl">Collection</h1>
+      <h2 class="text-center pt-10 font-made-okine-sans-personal-use font-light text-Blanc sm:text-sm lg:text-base xl:text-base">Vos dernières créations.</h2>
+      <ListeMontres class="flex flex-wrap gap-2" :max="3" />
+      <RouterLink class="text-Blanc underline font-made-okine-sans-personal-use font-light" to="/montre/liste">Voir tout</RouterLink>
+    </section>
+  </div>
 </template>
