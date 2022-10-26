f28e8e1a14ec04455fee96f8c7d57bd937ed9477
MercierMaxime <maxime.mercier@edu.univ-fcomte.fr>
2022-10-26 13:41:13 +0200

    Fonction Collection fonctionnel

--- src/pages/montre/index.vue
+++ src/pages/montre/index.vue
@@ -8,12 +8,10 @@ import ListeMontres from "@/components/ListeMontres.vue";
     <section v-if="user">
       <h1 class="text-Blanc text-center font-made-okine-sans-personal-use font-regular sm:text-lg lg:text-xl xl:text-4xl">Collection</h1>
       <h2 class="text-center pt-10 pb-20 font-made-okine-sans-personal-use font-light text-Blanc sm:text-sm lg:text-base xl:text-base">Vos dernières créations.</h2>
-      <div class="flex flex-wrap">
-        <div class="w-64 m-auto grid grid-cols-3">
-        <ListeMontres class="flex flex-wrap gap-2" :max="3" />
+          <ListeMontres class="flex flex-wrap justify-center gap-2" :max="3" />
+        <div class="pt-40 pb-10 flex justify-center">
+          <RouterLink class="text-center px-20 py-5 rounded-xl border-4 border-solid border-Blanc text-Blanc font-made-okine-sans-personal-use font-light sm:text-base lg:text-lg xl:text-xl" to="/montre/liste">Voir tout</RouterLink>
         </div>
-      </div>
-      <RouterLink class="text-Blanc underline font-made-okine-sans-personal-use font-light" to="/montre/liste">Voir tout</RouterLink>
     </section>
   </div>
 </template>
--- src/pages/montre/liste.vue
+++ src/pages/montre/liste.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import ListeMontres from '@/components/ListeMontres.vue';
 import { supabase, user } from '@/supabase';
-
 </script>
 
-
 <template>
-<section v-if="user">
-    <h1>Un aperçu de vos Montres : (<RouterLink class="text-red-300 underline" to="/montre/liste">Voir tout</RouterLink>)</h1>
-    <ListeMontres class="flex flex-wrap gap-2" />
-  </section>
+  <div class="bg-Noir">
+    <section v-if="user">
+    <h1 class="text-Blanc text-center font-made-okine-sans-personal-use font-regular sm:text-lg lg:text-xl xl:text-4xl">Liste</h1>
+    <h2 class="text-center pt-10 pb-20 font-made-okine-sans-personal-use font-light text-Blanc sm:text-sm lg:text-base xl:text-base">Toutes vos montres.</h2>
+      <ListeMontres class="flex flex-wrap gap-2 pb-14" />
+    </section>
+  </div>
 </template>
